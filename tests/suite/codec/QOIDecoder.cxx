bf7d3265e4c1c17b45dc396e4feb02e22797236e
Adam Halim <adaab301@student.liu.se>
2023-04-25 10:09:02 +0200

    Add QOIDecoder
    Seems to perform ~2x as good as PNGDecoder for encoding images.
    Also faster when decoding.

---
+++ tests/suite/codec/QOIDecoder.cxx
@@ -0,0 +1,80 @@
+#include "QOIDecoder.h"
+#include "lib/fpng.h"
+
+#define QOI_IMPLEMENTATION
+#include "lib/qoi.h"
+
+namespace suite {
+
+ QOIDecoder::QOIDecoder()
+ {
+ }
+ 
+ QOIDecoder::~QOIDecoder()
+ {
+ }
+
+ Image* QOIDecoder::decodeImageFromFile(std::string filename)
+ {
+  qoi_desc desc;
+  void* data = qoi_read(filename.c_str(), &desc, 4);
+
+  if (!data)
+    throw std::ios_base::failure("qoi: error decoding image");
+
+  Image* image = new Image(desc.width, desc.height);
+  image->setBuffer((rdr::U8*)data, desc.width * desc.height * desc.channels);
+  measureFPS();
+  return image;
+ }
+
+Image* QOIDecoder::decodeImageFromMemory(rdr::U8* data, int width, int height,
+                                          int size, int x_offset, int y_offset)
+{
+  qoi_desc desc;
+  rdr::U8* decodedImage = (rdr::U8*)qoi_decode(data, size, &desc, 4);
+  if (decodedImage == NULL)
+    throw std::ios_base::failure("qoi: error decoding image");
+
+  Image* image = new Image(width, height, x_offset, y_offset);
+  image->setBuffer(decodedImage, desc.width * desc.height * desc.channels);
+  measureFPS();
+  return image;
+}
+ void QOIDecoder::encodeImageTofile(const rdr::U8 *data, int width, int height,
+                                    std::string filename)
+ {
+  qoi_desc desc{
+    .width = (unsigned int)width,
+    .height = (unsigned int)height,
+    .channels = 4,
+    .colorspace = QOI_SRGB
+  };
+
+  int size = qoi_write(filename.c_str(), data, &desc);
+
+  if (!size)
+    throw std::ios_base::failure("error encoding image");
+
+  measureFPS();
+ }
+
+ Image* QOIDecoder::encodeImageToMemory(const rdr::U8 *data, int width,
+                                        int height, int x_offset, int y_offset)
+ {
+  qoi_desc desc{
+    .width = (unsigned int)width,
+    .height = (unsigned int)height,
+    .channels = 4,
+    .colorspace = QOI_SRGB
+  };
+
+  int size;
+  rdr::U8* encodedData = (rdr::U8*)qoi_encode(data, &desc,  &size);
+
+  Image* image = new Image(desc.width, desc.height);
+  image->setBuffer(encodedData, size);
+  measureFPS();
+  return image;
+ }
+}
