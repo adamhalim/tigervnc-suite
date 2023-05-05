f083bd59acf94aebf48fe723c1ac9fb9db58bff3
Adam Halim <adaab301@student.liu.se>
2023-05-05 13:52:59 +0200

    Manager::stats map instead of vector
    We use EncoderClass as index so that no conflicts occur. Previously,
    TightEncoder would be ovewritten by TightJPEGEncoder as they both return
    Encoder.encoding = 7;

--- tests/suite/Manager.cxx
+++ tests/suite/Manager.cxx
@@ -28,23 +28,34 @@ namespace suite {
     encoders[encoderTightJPEG] = new TimedTightJPEGEncoder(conn);
     encoders[encoderZRLE] = new TimedZRLEEncoder(conn);
 
-    for(Encoder* e : encoders) {
-      TimedEncoder* timedEncoder = dynamic_cast<TimedEncoder*>(e);
-      assert(timedEncoder != NULL);
-      timedEncoders.push_back(timedEncoder);
-    }
+
+    // Since Tight & TightJPEG share the same encode value (7), 
+    // we can't just loop through the encoders. We use EncoderClass as an 
+    // identifier instead of Encoder.encoding
+    timedEncoders[encoderRaw] =
+             dynamic_cast<TimedEncoder*>(encoders[encoderRaw]);
+    timedEncoders[encoderRRE] = 
+              dynamic_cast<TimedEncoder*>(encoders[encoderRRE]);
+    timedEncoders[encoderHextile] =
+              dynamic_cast<TimedEncoder*>(encoders[encoderHextile]);
+    timedEncoders[encoderTight] =
+              dynamic_cast<TimedEncoder*>(encoders[encoderTight]);
+    timedEncoders[encoderTightJPEG] = 
+              dynamic_cast<TimedEncoder*>(encoders[encoderTightJPEG]);
+    timedEncoders[encoderZRLE] =
+              dynamic_cast<TimedEncoder*>(encoders[encoderZRLE]);
   }
 
   Manager::~Manager()
   {
   }
 
-  std::vector<encoderStats> Manager::stats() {
-    std::vector<encoderStats> stats;
-    for(TimedEncoder* encoder : timedEncoders) {
-      if (encoder->stats().writeRectEncodetime ||
-          encoder->stats().writeSolidRectEncodetime)
-        stats.push_back(encoder->stats());  
+  std::map<const int, encoderStats> Manager::stats() {
+    std::map<const int, encoderStats> stats;
+    for(auto const& encoder : timedEncoders) {
+      if (encoder.second->stats().writeRectEncodetime ||
+          encoder.second->stats().writeSolidRectEncodetime)
+          stats[encoder.first] = encoder.second->stats();
     }
     return stats;
   }
